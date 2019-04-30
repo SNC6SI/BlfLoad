bf075899260d80dce72203ac3a01fd89283807e6
Chenghao Shen <shingosch@hotmail.com>
2019-04-30 15:44:28 +0800

    Add functionality: Output canmsgid and canchannel.

--- BlfExtractor.c
+++ BlfExtractor.c
@@ -31,7 +31,7 @@ int read_statistics(LPCTSTR pFileName, VBLFileStatisticsEx* pstatistics)
 
 
 
-int read_info( LPCTSTR pFileName, LPDWORD pRead, double* b)
+int read_info( LPCTSTR pFileName, LPDWORD pRead, double* candata_, double* canmsgid_, double* canchannel_)
 {
     HANDLE hFile;
     VBLObjectHeaderBase base;
@@ -70,7 +70,9 @@ int read_info( LPCTSTR pFileName, LPDWORD pRead, double* b)
             bSuccess = BLReadObjectSecure( hFile, &message.mHeader.mBase, sizeof(message));
             /* free memory for the CAN message */
             if( bSuccess) {
-              for(i=0;i<8;i++) *(b + (*pRead)*8 + i) = message.mData[i];
+              for(i=0;i<8;i++) *(candata_ + (*pRead)*8 + i) = message.mData[i];
+              *(canmsgid_ + (*pRead)) = message.mID;
+              *(canchannel_ + (*pRead)) = message.mChannel;
               BLFreeObject( hFile, &message.mHeader.mBase);
               *pRead += 1;
             }
@@ -81,7 +83,9 @@ int read_info( LPCTSTR pFileName, LPDWORD pRead, double* b)
             bSuccess = BLReadObjectSecure( hFile, &message2.mHeader.mBase, sizeof(message2));
             /* free memory for the CAN message */
             if( bSuccess) {
-              for(i=0;i<8;i++) *(b + (*pRead)*8 + i) = message2.mData[i];
+              for(i=0;i<8;i++) *(candata_ + (*pRead)*8 + i) = message2.mData[i];
+              *(canmsgid_ + (*pRead)) = message2.mID;
+              *(canchannel_ + (*pRead)) = message2.mChannel;
               BLFreeObject( hFile, &message2.mHeader.mBase);
               *pRead += 1;
             }
@@ -112,18 +116,33 @@ int read_info( LPCTSTR pFileName, LPDWORD pRead, double* b)
 void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
     LPCTSTR pFileName = _T( "chan124_new.blf");
     DWORD dwRead;
-    double *ptr;
+    double *candata, *canmsgid, *canchannel, cantime;
     
     int result = 0;
     
     VBLFileStatisticsEx statistics = { sizeof( statistics)};
     result = read_statistics( pFileName, &statistics);
 
-    
+    // plhs[0]: candata
     plhs[0] = mxCreateDoubleMatrix (8,statistics.mObjectCount , mxREAL);
-    ptr = mxGetPr(plhs[0]);
-    result = read_info( pFileName, &dwRead, ptr);
+    candata = mxGetPr(plhs[0]);
+    
+    // plhs[1]: canmsgid
+    plhs[1] = mxCreateDoubleMatrix (1,statistics.mObjectCount , mxREAL);
+    canmsgid = mxGetPr(plhs[1]);
+    
+    // plhs[2]: canchannel
+    plhs[2] = mxCreateDoubleMatrix (1,statistics.mObjectCount , mxREAL);
+    canchannel = mxGetPr(plhs[2]);
+    
+    // plhs[3]: camtime
+    //plhs[3] = mxCreateDoubleMatrix (1,statistics.mObjectCount , mxREAL);
+    //cantime = mxGetPr(plhs[3]);
+    
+    result = read_info( pFileName, &dwRead, candata, canmsgid, canchannel);
     mxSetN(plhs[0],dwRead);
+    mxSetN(plhs[1],dwRead);
+    mxSetN(plhs[2],dwRead);
   
 }
 
